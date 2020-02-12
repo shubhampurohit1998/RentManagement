2070b0e1cca82054a9362f70b55666ced5afde5b
shubhampurohit1998 <spurohit@bestpeers.com>
2020-02-12 14:37:44 +0530

    Change in model

---
+++ UserApp/forms.py
@@ -0,0 +1,6 @@
+from django.forms import ModelForm
+from django.forms import Form
+from .models import *
+
+
+class RegisterForm(ModelForm):
--- UserApp/views.py
+++ UserApp/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 
 def index(request, ownr_id):
     ownr = get_object_or_404(Owner, id=ownr_id)
-    return render(request, 'index.html', {'ownr':ownr })
+    return render(request, 'index.html', {'ownr': ownr})
 
