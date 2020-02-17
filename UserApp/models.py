e8c434cea916c36c2ff0a0308d5bf746a5ffdbc0
shubhampurohit1998 <spurohit@bestpeers.com>
2020-02-17 19:35:16 +0530

    Change in model

--- UserApp/models.py
+++ UserApp/models.py
@@ -24,14 +24,15 @@ class Property(models.Model):
         ('flat', 'Flat'),
     ]
     owner = models.ForeignKey(Person, on_delete=models.CASCADE)
-    price = models.CharField(max_length=30)
-    size = models.CharField(max_length=30)
-    address = models.CharField(max_length=100)
+    price = models.CharField(max_length=30, verbose_name='Price')
+    size = models.CharField(max_length=30, verbose_name='Size')
+    address = models.CharField(max_length=100, verbose_name='Address')
     p_type = models.CharField(
         max_length=8,
         choices=PROPERTY_TYPE,
     )
-    city = models.CharField(max_length=30, null=False, blank=False)
+    city = models.CharField(max_length=30, null=False, blank=False, verbose_name='City')
+    description = models.TextField(max_length=150, blank=True, null=True, verbose_name='Description')
 
     def __str__(self):
         return unicode(self.p_type)  # f-string python 3 string concatanation
@@ -40,8 +41,8 @@ class Property(models.Model):
 class Rent(models.Model):
     property = models.OneToOneField(Property, on_delete=models.CASCADE)
     customer = models.ForeignKey(Person, models.SET_NULL, blank=True, null=True)
-    date_on_rent = models.DateField(null=False)
-    tenure = models.DateField(blank=True, null=True)
+    date_on_rent = models.DateField(null=False, verbose_name='Rent start date')
+    tenure = models.DateField(blank=True, null=True, verbose_name='Tenure of rent')
 
     def __str__(self):
         return unicode(self.customer)
@@ -49,4 +50,4 @@ class Rent(models.Model):
 
 class Picture(models.Model):
     property = models.ForeignKey(Property, on_delete=models.CASCADE)
-    pic_name = models.ImageField(upload_to='images/')
+    pic_name = models.ImageField(upload_to='images/', verbose_name='Picture')
